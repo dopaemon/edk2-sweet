99b7d992837476263f004bcb3092f3163f522148
Cl1nkkk <103186001+Cl1nkkk@users.noreply.github.com>
2022-04-28 19:39:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -e
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM8250/SM8250.dsc
 gzip -c < workspace/Build/SM8250/DEBUG_GCC5/FV/SM8250_UEFI.fd >uefi.img
 cat k20.dtb >>uefi.img
-$(which "abootimg") --create boot_uefi.img -k uefi.img -r androidboot/ramdisk -f androidboot/android.cfg
+$(which "abootimg") --create k20_uefi.img -k uefi.img -r androidboot/ramdisk -f androidboot/android.cfg
 rm -r uefi.img workspace/*
 #dd if=uefi_out.img of=/dev/block/by-name/recovery
