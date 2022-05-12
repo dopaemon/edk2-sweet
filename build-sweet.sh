96cb3c5c6661152f6fc7e6c695b93daf88777b1c
Cl1nkkk <103186001+Cl1nkkk@users.noreply.github.com>
2022-05-12 12:45:10 +0300

    Update build-sweet.sh

--- build-sweet.sh
+++ build-sweet.sh
@@ -3,7 +3,7 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM8250/SM8250.dsc
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM8250/sweet.dsc
 gzip -c < workspace/Build/SM8250/DEBUG_GCC5/FV/SM8250_UEFI.fd >uefi.img
 cat sweet.dtb >>uefi.img
 $(which "abootimg") --create sweet_uefi.img -k uefi.img -r androidboot/ramdisk -f androidboot/android.cfg
